b868cf5310c5d0b1c32f13ed603a1738d13b28c7
Gaige Pierce-Raison <gaige@fraud.net>
2017-11-19 21:32:18 -0800

    Small change to rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 CONTAINER=gaigepr/sbcl-1.4.1
-docker rm -f $(docker ps -a -q); docker build -t $CONTAINER . && docker run -it $CONTAINER
+docker build -t $CONTAINER . && docker run -it $CONTAINER
